bdbc4f23569230762b93a78d958960ffc9455a4d
Jojo Slagle <SteeledSlagle13@gmail.com>
2018-05-01 14:31:00 -0500

    Date support
    changed from string objs to dates

--- SQL-Scripts/InventoryCreate.sql
+++ SQL-Scripts/InventoryCreate.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS Inventory;
 CREATE TABLE Inventory (
   szProductID    VARCHAR(45) NOT NULL,
   iQuantity      INT(100)    NOT NULL DEFAULT 0,
-  szRecievedDt   VARCHAR(45) NOT NULL DEFAULT '00-00-0000',
-  szExpirationDt VARCHAR(45) NOT NULL DEFAULT '00-00-0000',
+  szRecievedDt   DATE        NOT NULL DEFAULT '1000-01-01',
+  szExpirationDt DATE        NOT NULL DEFAULT '1000-01-01',
   PRIMARY KEY (szProductID)
 );
