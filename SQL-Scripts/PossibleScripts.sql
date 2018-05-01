932c31b07f5af5793bdf485f106cd66be2901fe5
Jojo Slagle <SteeledSlagle13@gmail.com>
2018-05-01 00:25:31 -0500

    Added the Query Jonathan requested #4

--- SQL-Scripts/PossibleScripts.sql
+++ SQL-Scripts/PossibleScripts.sql
@@ -1,3 +1,4 @@
+--1
 -- Select the Product where the price is greater than PUT_DOUBLE_HERE
 -- Replace PUT_DOUBLE_HERE with a double
 -- Can be reversed to grab products less than PUT_DOUBLE_HERE by changing > to <
@@ -10,6 +11,7 @@ FROM
 WHERE
   p.dbPrice > PUT_DOUBLE_HERE;
 
+--2
 -- Select Product name and ID with a ORDER NUMBER
 SELECT 
   p.szProductID, p.szProductName
@@ -20,6 +22,7 @@ WHERE
   o.szOrderID = 'ORDER_ID'
     AND o.szProductID =  p.szProductID;
 
+--3
 -- Select the Inventory and the Product name
 -- Name is from the Product table
 SELECT 
@@ -34,3 +37,26 @@ FROM
   Inventory i
 WHERE
   i.szProductID = p.szProductID;
+ 
+ --4
+ -- Get the Total Cost, Total Items, Order Date, Order ID
+ -- The most annoying Query I have written.... So faaaaar
+ SELECT 
+    t1.szOrderID,
+    t1.szOrderDT,
+    SUM(dbTotalPrice) AS dbTotalCost,
+    SUM(iTotalQty) AS iTotalItems
+FROM
+    (SELECT 
+        oo.szProductID,
+            oo.szOrderID,
+            oo.szOrderDT,
+            SUM(oo.iQuantity) * pp.dbPrice AS dbTotalPrice,
+            SUM(oo.iQuantity) AS iTotalQty
+    FROM
+        Orders oo, Product pp
+    WHERE
+        oo.szProductID = pp.szProductID
+    GROUP BY oo.szOrderID , oo.szProductID , oo.szOrderDT) t1
+GROUP BY t1.szOrderID , t1.szOrderDT;
+
